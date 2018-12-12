46643b706ccc6360aa3b68baa10829d8052122a8
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2018-12-12 15:46:14 +0100

    revert changes in registering

--- src/performance-monitoring/index.js
+++ src/performance-monitoring/index.js
@@ -4,14 +4,15 @@ var TransactionService = require('./transaction-service')
 module.exports = {
   PerformanceMonitoring: PerformanceMonitoring,
   registerServices: function registerServices (serviceFactory) {
-    var configService = serviceFactory.getService('ConfigService')
-    var loggingService = serviceFactory.getService('LoggingService')
-
     serviceFactory.registerServiceCreator('TransactionService', function () {
+      var configService = serviceFactory.getService('ConfigService')
+      var loggingService = serviceFactory.getService('LoggingService')
       return new TransactionService(loggingService, configService)
     })
 
     serviceFactory.registerServiceCreator('PerformanceMonitoring', function () {
+      var configService = serviceFactory.getService('ConfigService')
+      var loggingService = serviceFactory.getService('LoggingService')
       var apmService = serviceFactory.getService('ApmServer')
       var zoneService
       var transactionService = serviceFactory.getService('TransactionService')
